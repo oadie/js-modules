b68fe1a3c1957f52aabecaf0c58c3e02d9075c7b
Diego O <ortega.a.die@gmail.com>
2021-10-04 22:24:46 -0500

    modules

---
+++ main.js
@@ -0,0 +1,4 @@
+import { suma, Hello } from "./math";
+
+cosole.log(suma(1,2));
+console.log(Hello);
---
+++ math.js
@@ -0,0 +1,5 @@
+import const suma = (a, b) => {
+    return a+b;
+}
+
+import const Hello = "hello, Diego!";
